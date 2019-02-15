5d593ca49b62fdf43dd7aab8340032aaaa346f71
Lagom92 <jyyt0147@gmail.com>
2019-02-15 10:14:08 +0900

    190215 | min_max

--- count/test.py
+++ count/test.py
@@ -5,9 +5,9 @@ for tc in range(1, 11):
     count = 0
     area = int(input())
     bd = list(map(int, input().split()))
-    for i in range(2, len(bd)):
+    for i in range(2, len(bd)-2):
            if bd[i-1] < bd[i] and bd[i] > bd[i+1] and bd[i-2] < bd[i] and bd[i] > bd[i+2]:
-               a = bd[i] - max([bd[i-2], bd[i-1], bd[i+1], bd[i+2]])
-               count += a
+               count += bd[i] - max([bd[i-2], bd[i-1], bd[i+1], bd[i+2]])
+
 
     print(f"#{tc} {count}")
