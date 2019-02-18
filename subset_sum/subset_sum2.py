f3bde1b8177d07c4fb40ecb5211bf86284dda835
Lagom92 <jyyt0147@gmail.com>
2019-02-18 14:56:19 +0900

    190218 | 부분집합

--- subset_sum/subset_sum2.py
+++ subset_sum/subset_sum2.py
@@ -16,3 +16,15 @@ for i in range(2**N):
         if i & (1 << j) != 0:  # i 의 j번 비트 검사, A[j]가 포함되면
             print(A[j], end="")
     print()
+
+# 부분집합의 합
+for i in range(2**N):
+    s = 0
+    # i를 비트 단위로 접근
+    for j in range(N):
+        if i & (1 << j) != 0:  # i 의 j번 비트 검사, A[j]가 포함되면
+            s += A[j]
+            print(A[j], end="")
+
+    print(s)
+    print()
