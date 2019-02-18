bb1f473e3e53b6f32545f04747821574c92e2657
Lagom92 <jyyt0147@gmail.com>
2019-02-18 14:52:04 +0900

    190218 | 부분집합

--- dec2bin/dec2bin.py
+++ dec2bin/dec2bin.py
@@ -1,6 +1,7 @@
+# 비트 검사
 a = 14
 for i in range(7, -1, -1):
-    if a&(1<<i) == 0:
+    if a & ( 1 << i) == 0:
         print(0, end="")
     else:
         print(1, end="")
@@ -8,7 +9,7 @@ for i in range(7, -1, -1):
 print()
 # 방식 2
 for i in range(7, -1, -1):
-    if a&(1<<i) != 0:
+    if a & ( 1 << i) != 0:
         print(1, end="")
     else:
         print(0, end="")
---
+++ subset_sum/subset_sum2.py
@@ -0,0 +1,18 @@
+# 부분집합 만들기
+A = [1, 2, 3]
+N = 3
+
+for i in range(2**N):
+    # i를 비트 단위로 접근
+    for j in range(N-1, -1, -1):
+        if i & (1 << j) != 0:  # i 의 j번 비트 검사, A[j]가 포함되면
+            print(A[j], end="")
+    print()
+
+# 방식2
+for i in range(2**N):
+    # i를 비트 단위로 접근
+    for j in range(N):
+        if i & (1 << j) != 0:  # i 의 j번 비트 검사, A[j]가 포함되면
+            print(A[j], end="")
+    print()
