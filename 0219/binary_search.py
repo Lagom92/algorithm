7e3fc65897d8521e2a9e540cc85d4ce4f27f2d2d
Lagom92 <jyyt0147@gmail.com>
2019-02-19 15:20:50 +0900

    190219 | 배열2 문제풀기

---
+++ 0219/binary_search.py
@@ -0,0 +1,39 @@
+import sys
+sys.stdin = open('input04.txt', 'r')
+
+T = int(input())
+for tc in range(1, T+1):
+    book, targetA, targetB = map(int, input().split())
+
+    left = 1
+    right = book
+    cntA = 0
+    while left < right:
+        mid = (left + right) // 2
+        cntA += 1
+        if mid == targetA:
+            break
+        elif mid > targetA:
+            right = mid
+        else:
+            left = mid
+
+    left = 1
+    right = book
+    cntB = 0
+    while left < right:
+        mid = (left + right) // 2
+        cntB += 1
+        if mid == targetB:
+            break
+        elif mid > targetB:
+            right = mid
+        else:
+            left = mid
+
+    if cntA < cntB:
+        print(f"#{tc} A")
+    elif cntA > cntB:
+        print(f"#{tc} B")
+    else:
+        print(f"#{tc} 0")
