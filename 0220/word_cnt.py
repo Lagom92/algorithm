ec7f89fc87a8de6ec2c825b220c4347e1180f91b
Lagom92 <jyyt0147@gmail.com>
2019-02-20 11:41:34 +0900

    190220 | 문자열

---
+++ 0220/word_cnt.py
@@ -0,0 +1,20 @@
+# 글자수 문제
+import sys
+sys.stdin = open('input03.txt', 'r')
+
+T = int(input())
+for tc in range(1, T+1):
+    str1 = input()
+    str2 = input()
+    result = 0
+
+    for i in str1:
+        cnt = 0
+        for j in str2:
+            if i == j:
+                cnt += 1
+        if result < cnt:
+            result = cnt
+
+    print(f"#{tc} {result}")
+
