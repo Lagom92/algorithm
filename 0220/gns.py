2019cf02fe2d54243baf6073639e9102ed63dda3
Lagom92 <jyyt0147@gmail.com>
2019-02-20 16:02:52 +0900

    190220 | 문자열

--- 0220/gns.py
+++ 0220/gns.py
@@ -1,3 +1,4 @@
+# GNS 문제
 import sys
 sys.stdin = open('GNS_test_input.txt', 'r')
 
@@ -9,9 +10,9 @@ for test_case in range(1, T+1):
     nums = ["ZRO", "ONE", "TWO", "THR", "FOR", "FIV", "SIX", "SVN", "EGT", "NIN"]
     result = ""
 
-    for i in range(10):
+    for i in range(10):  # nums 길이 만큼 실행
         cnt = 0
-        if nums[i] in word:
+        if nums[i] in word:  # nums의 인덱스를 이용해서 단어가 word 에 있는지 확인
             if nums[i] not in result:
                 cnt = word.count(nums[i])
                 result += (nums[i]+" ") * cnt
