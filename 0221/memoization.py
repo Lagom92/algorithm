8058f49498cfe8781dbaa1aa21abd2800242dc2c
Lagom92 <jyyt0147@gmail.com>
2019-02-21 13:55:29 +0900

    190221 | stack

--- 0221/memoization.py
+++ 0221/memoization.py
@@ -2,7 +2,7 @@
 # 피보나치
 
 def f(n):
-    if n>= 2 and memo[n] == 0:
+    if n >= 2 and memo[n] == 0:
         memo[n] = f(n-1) + f(n-2)
     return memo[n]
 
@@ -13,3 +13,14 @@ memo[1] = 1
 
 print(f(N))
 print(memo)
+
+
+# 방법 2
+
+F = [0] * (N+1)
+F[0] = 0
+F[1] = 1
+for n in range(2, N+1):
+    F[n] = F[n-1] + F[n-2]
+
+print(F[n])
