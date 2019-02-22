adb587c40bcae75365cc388bb11335fa80b1ec59
Lagom92 <jyyt0147@gmail.com>
2019-02-22 15:16:41 +0900

    190222 | 회문 study

---
+++ 0222/compare.py
@@ -0,0 +1,19 @@
+# 문자열 비교
+import sys
+sys.stdin = open('input01.txt', 'r')
+
+T = int(input())
+for tc in range(1, T+1):
+    str1 = input()
+    str2 = input()
+
+    n1 = len(str1)
+    n2 = len(str2)
+    cnt = 0
+
+    for i in range(0, n2-n1+1):
+        if str2[i] == str1[0]:
+            if str1 == str2[i:i+n1]:
+                cnt = 1
+
+    print(f"#{tc} {cnt}")
--- 0222/palindrome2.py
+++ 0222/palindrome2.py
@@ -1,1 +1,34 @@
 # 회문2
+import sys
+sys.stdin = open('input.txt', 'r')
+
+def pal(N, M, string):
+    for i in range(N):  # 0 -> N-1 까지
+        for j in range(N - M + 1):  # 0 -> N-M 까지
+            a = ""
+            b = ""
+            a = "".join(string[i])[j:j + M]
+            if a == a[::-1]:  # 회문 이면
+                return a
+            for k in range(M):
+                b += "".join(string[j + k])[i]
+            if b == b[::-1]:  # 회문이면
+                return b
+
+for test_case in range(10):
+    tc = int(input())
+    arr = [list(input().split()) for i in range(100)]
+
+    max_cnt = 0
+    result = ""
+    for i in range(100, -1, -1):
+        a = pal(100, i, arr)
+        if a != None:
+            result = a
+            break
+
+    print(f"#{tc} {len(result)}")
+
+
+
+
