96e64f18376e32b3e9c394dfc4e38608f40060a8
Lagom92 <jyyt0147@gmail.com>
2019-03-11 13:35:17 +0900

    190311 | 알고리즘

--- 0311/linked_list.py
+++ 0311/linked_list.py
@@ -5,10 +5,20 @@ class Node:
         self.item = item
         self.link = n
 
-def addFirst(data):
+def addFirst(data):   # Head 바로 다음에 data를 넣음
     global Head
     Head = Node(data, Head)
 
+def search(data):   # data가 들어있는 Node의 주소를 리턴
+    global Head
+    p = Head
+    while p != None and p.item != data:   # 노드는 존재하지만 찾는 값을 갖고있지 않으면
+        p = p.link   # 다음 노드로 이동
+    return p
+
+def add(data1, data2):   # data1 뒤에 data2 노드 추가
+    p = search(data1)
+    p.link = Node(data2, p.link)
 
 Head = None
 addFirst(10)
@@ -22,5 +32,23 @@ print(Head.link.item)
 # 20
 print(Head.link.link.item)
 # 10
+print(Head.link.link.link)
+# None
 
+print(Head.link)   # 아래와 같음
+print(search(20))
+print("-----")
+
+add(20,15)
+
+print(Head.link.item)
+# 20
+print(Head.link.link.item)
+# 15
+
+print("====")
+p = Head
+while p != None:
+    print(p.item)
+    p = p.link
 
