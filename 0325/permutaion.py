2971fd67b35cec3d507ab503145f0f41c26f2ae5
Lagom92 <jyyt0147@gmail.com>
2019-03-25 11:24:57 +0900

    190325 | 알고리즘

---
+++ 0325/permutaion.py
@@ -0,0 +1,11 @@
+def npr(n, k):   # n이 순열의 길이, k는 숫자를 결정할 자리
+    if n == k:
+        print(p)
+    else:
+        for i in range(k, n):
+            p[i], p[k] = p[k], p[i]
+            npr(n, k+1)
+            p[i], p[k] = p[k], p[i]
+
+p = [1,2,3]
+npr(3,0)
